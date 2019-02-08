5b5509da36ddada62e372c1a6008a02b2388c569
niconico <niconiconico.0113@gmail.com>
2019-02-08 09:56:44 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@
 Route::get('/', 'TasksController@index'); 
 Route::resource('tasks', 'TasksController');
 //ユーザー登録
-Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->('signup.get');
-Route::post('signup','Auth\RegisterController')->name('signup.post');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 //ログイン認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+//ユーザー機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
